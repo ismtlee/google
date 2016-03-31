de08293c19f3514718c00b5ff8097b644794373c
Smart Lee <ismtlee@gmail.com>
2016-03-31 09:50:03 +0800

    .

--- android_publish/env.php
+++ android_publish/env.php
@@ -1,12 +1,13 @@
 <?php
 define('APP_PATH', dirname(__FILE__));
+define('LIB_PATH', dirname(dirname(__FILE__)));
 define('CONFIG_INI', APP_PATH . '/conf/application.ini');
 
-set_include_path(APP_PATH . "/google-api-php-client/src" . PATH_SEPARATOR .
+set_include_path(LIB_PATH . "/google-api-php-client/src" . PATH_SEPARATOR .
   APP_PATH . "/library" . PATH_SEPARATOR .
-  APP_PATH . "/googleads-adsense-examples/php-clientlib-1.x/v1.x/". PATH_SEPARATOR. get_include_path());
+  get_include_path());
 
-require_once 'templates/base.php';
+#require_once 'templates/base.php';
 require_once 'Google/autoload.php';
 
 
--- android_publish/main.php
+++ android_publish/main.php
@@ -3,15 +3,22 @@ require_once 'env.php';
 
 $client = new Google_Client();
 #$client->addScope('https://www.googleapis.com/auth/adsense.readonly');
-$client->setScopes(array('https://www.googleapis.com/auth/androidpublisher'));
+#$client->setScopes(array('https://www.googleapis.com/auth/androidpublisher'));
 $client->setAccessType('offline');
 
-$client->setAuthConfigFile(APP_PATH.'/client_secrets.json');
-$refresh_token = $accounts_info['tokens'];
-$client->refreshToken($refresh_token);
-$access_token = $client->getAccessToken();
-$client->setAccessToken($access_token);
+$scopes = array('https://www.googleapis.com/auth/androidpublisher');
+$cred = $client->loadServiceAccountJson(APP_PATH.'/oauth2service.json', $scopes);
+$client->setAssertionCredentials($cred);
+#$client->setAuthConfigFile(APP_PATH.'/oauth2service.json');
+#$refresh_token = $accounts_info['tokens'];
+#$client->refreshToken($refresh_token);
+#$access_token = $client->getAccessToken();
+#$client->setAccessToken($access_token);
 
-$service = new Google_Service_AdSense($client);
+#$service = new Google_Service_AdSense($client);
+$service = new Google_Service_AndroidPublisher($client); 
 
+#$service->edits->get('com.iendlessrun.tombrushtempleescape', 2);
+print_r($service->edits);
+// print_r($service);
 
